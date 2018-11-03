e34386a24dbf917d3344cc97f698b309a1c48f78
Kristof Van Aken <vanakenk@gmail.com>
2018-11-03 21:22:03 +0100

    Stateless components & implicit returns

--- Advancedreact/catch-of-the-day/src/components/Header.js
+++ Advancedreact/catch-of-the-day/src/components/Header.js
@@ -1,25 +1,21 @@
 import React from 'react';
 
-class Header extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <header className="top">
-                    <h1>Catch
-                        <span className="ofThe">
+const Header = ({tagline, age}) =>  (
+        <React.Fragment>
+            <header className="top">
+                <h1>Catch
+                    <span className="ofThe">
                             <span className="of">Of</span>
                             <span className="the">The</span>
                         </span>
-                        Day
-                    </h1>
+                    Day
+                </h1>
 
-                    <h3 className="tagline">
-                        <span>Fresh Seafood Market</span>
-                    </h3>
-                </header>
-            </React.Fragment>
-        );
-    }
-}
+                <h3 className="tagline">
+                    <span>{tagline}</span>
+                </h3>
+            </header>
+        </React.Fragment>
+    );
 
 export default Header;
